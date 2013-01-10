4f06a347c2be60ebc8d7a856c50153687ec69522
skywave <thomaskoch@gmail.com>
2013-01-10 14:08:29 +0100

    Update opentripplanner-routing/src/main/java/org/opentripplanner/gtfs/GtfsLibrary.java
    Support all HGV route_types. Some already supported by Google Transit and also in some official feeds (Berlin OpenVBB)
    Source of the list: https://groups.google.com/forum/?fromgroups=#!msg/gtfs-changes/keT5rTPS7Y0/71uMz2l6ke0J

--- opentripplanner-routing/src/main/java/org/opentripplanner/gtfs/GtfsLibrary.java
+++ opentripplanner-routing/src/main/java/org/opentripplanner/gtfs/GtfsLibrary.java
@@ -97,6 +97,34 @@ public class GtfsLibrary {
     }
 
     public static TraverseMode getTraverseMode(Route route) {
+        int routeType = route.getType();
+        if (routeType >= 100 && routeType < 200){ // Railway Service
+            return TraverseMode.RAIL;
+        }else if (routeType >= 200 && routeType < 300){ //Coach Service
+            return TraverseMode.BUS;
+        }else if (routeType >= 300 && routeType < 500){ //Suburban Railway Service and Urban Railway service
+            return TraverseMode.RAIL;
+        }else if (routeType >= 500 && routeType < 700){ //Metro Service and Underground Service
+            return TraverseMode.SUBWAY;
+        }else if (routeType >= 700 && routeType < 900){ //Bus Service and Trolleybus service
+            return TraverseMode.BUS;
+        }else if (routeType >= 900 && routeType < 1000){ //Tram service
+            return TraverseMode.TRAM;
+        }else if (routeType >= 1000 && routeType < 1100){ //Water Transport Service
+            return TraverseMode.FERRY;
+        }else if (routeType >= 1100 && routeType < 1200){ //Air Service
+            throw new IllegalArgumentException("Air transport not supported" + route.getType());
+        }else if (routeType >= 1200 && routeType < 1300){ //Ferry Service
+            return TraverseMode.FERRY;
+        }else if (routeType >= 1300 && routeType < 1400){ //Telecabin Service
+            return TraverseMode.GONDOLA;
+        }else if (routeType >= 1400 && routeType < 1500){ //Funicalar Service
+            return TraverseMode.FUNICULAR;
+        }else if (routeType >= 1500 && routeType < 1600){ //Taxi Service
+            throw new IllegalArgumentException("Taxi service not supported" + route.getType());
+        }else if (routeType >= 1600 && routeType < 1700){ //Self drive
+            return TraverseMode.CAR;
+        }
         switch (route.getType()) {
         case 0:
         case 900:
@@ -110,7 +138,6 @@ public class GtfsLibrary {
         case 109:
             return TraverseMode.RAIL;
         case 3:
-        case 700:
             return TraverseMode.BUS;
         case 1000:
         case 4:
